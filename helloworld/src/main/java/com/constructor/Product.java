aad5cae9054314d01e3c5fb0499273d5078d6dfb
Tran Thanh Phong <Phong@Macs-MacBook-Pro.local>
2015-05-20 21:25:35 +0700

    This is first project

---
+++ helloworld/src/main/java/com/constructor/Product.java
@@ -0,0 +1,23 @@
+package com.constructor;
+
+/**
+ * Created by Phong on 5/20/15.
+ */
+public class Product {
+
+    private String name  = "" ;
+
+
+
+
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
